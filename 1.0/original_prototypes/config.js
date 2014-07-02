1adc0d9e0bb20254210e262734fd49aff9af1e9a
maria14 <mdiaz_calvo@hotmail.com>
2014-07-02 09:27:45 +0200

    CastleDefense and Memory added
    added two games Memory and CastleDefense and modified the  Report Sample

---
+++ 1.0/original_prototypes/config.js
@@ -0,0 +1,9 @@
+//globals: equal, responseText, statement, ok, deepEqual, QUnit, module, asyncTest, Util, start, golfStatements, console
+/*jslint bitwise: true, browser: true, plusplus: true, maxerr: 50, indent: 4 */
+function Config() {
+	"use strict";
+}
+Config.endpoint = "http://localhost:8000/xapi/";
+Config.user = "adllrs";
+Config.password = "asd";
+Config.actor = { "mbox":"ricar.fuenla@gmail.com", "name":"ricardo" };
